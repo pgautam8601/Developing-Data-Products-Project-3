7961eb53b0b856349aa81c71b1da369694023eeb
pgautam8601 <65444978+pgautam8601@users.noreply.github.com>
2020-06-21 15:04:03 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,37 +1,24 @@
-# Coursera - Developing Data Products- Course Project
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
 
-# ui.R file for the shiny app
+library(shiny)
 
-# This app was developed to help people choose the best car for their trip, using mtcars dataset, from [R] 
-
-library(markdown)
-
-shinyUI(navbarPage("Select the best car for your trip",
-        tabPanel("Table",
-                   
-        # Sidebar
-        sidebarLayout(
-            sidebarPanel(
-                helpText("Provide information about your trip and the carachteristics of the car that you want"),
-                numericInput('dis', 'Distance (in miles):', 50, min = 1, max = 1000),
-                numericInput('cost', 'Gasoline Price (per gallon):', 2.41, min = 2, max = 4, step=0.01),
-                numericInput('gas', 'Maximum expenditure on gasoline:', 50, min=1, max=1000),
-                checkboxGroupInput('cyl', 'Number of cylinders:', c("Four"=4, "Six"=6, "Eight"=8), selected = c(4,6,8)),
-                sliderInput('disp', 'Displacement', min=70, max=480, value=c(70,480), step=10),
-                sliderInput('hp', 'Gross horsepower', min=50, max=340, value=c(50,340), step=10),
-                checkboxGroupInput('am', 'Transmission:', c("Automatic"=0, "Manual"=1), selected = c(0,1))
-            ),
-            
-            
-            mainPanel(
-               dataTableOutput('table')
-            )
-        )
-),
-        tabPanel("About",
-            mainPanel(
-                includeMarkdown("about.md")
-            )
-        )
+shinyUI(pageWithSidebar(
+    headerPanel("Calculate your Body Mass Index (BMI)"),
+    sidebarPanel(
+        h5("BMI is a measure of body fat based on your height and weight that applies to adult men and women"),
+        numericInput("height", "Input your height (cm)", value=177.8),
+        numericInput("weight", "Input your weight (kg)", value=122.6),
+        submitButton("Calculate")
+    ),
+    mainPanel(
+        h3("Results:"),
+        p("Your BMI:"),
+        verbatimTextOutput("bmi"),
+        p("This is considered:"),
+        verbatimTextOutput("diagnostic")
     )
-)   
+))
